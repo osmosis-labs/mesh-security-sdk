7a9cb355c429a1fdc1899d3f447104f68852f280
Anmol1696 <anmol1696@gmail.com>
2023-07-08 17:00:22 +0200

    Update devnet setup for starship multi-node setup

---
+++ tests/starship/setup/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
+	starship "github.com/cosmology-tech/starship/clients/go/client"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/osmosis-labs/mesh-security-sdk/demo/app"
+	"go.uber.org/zap"
+	"gopkg.in/yaml.v3"
+	"os"
+	"time"
+)
+
+func MeshSecurity() error {
+	// read config file from yaml
+	yamlFile, err := os.ReadFile(configFile)
+	if err != nil {
+		return err
+	}
+	config := &starship.Config{}
+	err = yaml.Unmarshal(yamlFile, config)
+	if err != nil {
+		return err
+	}
+
+	// create chain clients
+	chainClients, err := starship.NewChainClients(zap.L(), config)
+	if err != nil {
+		return err
+	}
+
+	var (
+		consumerChain, _ = chainClients.GetChainClient("consumer")
+		providerChain, _ = chainClients.GetChainClient("provider")
+	)
+
+	// create Client
+	mm := []module.AppModuleBasic{}
+	for _, am := range app.ModuleBasics {
+		mm = append(mm, am)
+	}
+	consumerClient, err := NewClient("consume-client", zap.L(), consumerChain, mm)
+	if err != nil {
+		return err
+	}
+	providerClient, err := NewClient("provider-client", zap.L(), providerChain, mm)
+	if err != nil {
+		return err
+	}
+
+	// setup contracts on both chains
+	consumerCli := NewConsumerClient(consumerClient)
+	consumerContracts, err := consumerCli.BootstrapContracts()
+	if err != nil {
+		return err
+	}
+	converterPortID := wasmkeeper.PortIDForContract(consumerContracts.converter)
+	providerCli := NewProviderClient(providerClient)
+
+	ibcInfo, err := consumerChain.GetIBCInfo("provider")
+	if err != nil {
+		return err
+	}
+
+	connectionID := ibcInfo.Chain_1.ConnectionId
+	providerContracts, err := providerCli.BootstrapContracts(connectionID, converterPortID)
+	if err != nil {
+		return err
+	}
+
+	// create channel between 2 chains for the given port and channel
+	cmdRunner, err := starship.NewCmdRunner(zap.L(), config)
+	if err != nil {
+		return err
+	}
+
+	consumerPortID := wasmkeeper.PortIDForContract(providerContracts.externalStaking)
+
+	cmd := fmt.Sprintf("hermes create channel --a-chain %s --a-connection %s --a-port %s --b-port %s --yes", "consumer", connectionID, converterPortID, consumerPortID)
+	err = cmdRunner.RunExec("provider-consumer", cmd)
+	if err != nil {
+		return err
+	}
+
+	// wait for initial packets to be transfered via IBC over
+	validators, err := stakingtypes.NewQueryClient(consumerClient.Client).Validators(context.Background(), &stakingtypes.QueryValidatorsRequest{
+		Status: "BOND_STATUS_BONDED",
+	})
+	if err != nil {
+		return err
+	}
+	myExtValidatorAddr := validators.Validators[0].OperatorAddress
+
+	// stake tokens from the client address
+	err = consumerClient.StakeTokens(myExtValidatorAddr, 5000000, "ustake")
+	if err != nil {
+		return err
+	}
+
+	// then the active set should be stored in the ext staking contract
+	// and contain all active validator addresses
+	qRsp := map[string]any{}
+	err = Eventually(
+		func() bool {
+			qRsp = providerCli.QueryExtStaking(Query{"list_remote_validators": {}})
+			v := qRsp["validators"].([]interface{})
+			if len(v) > 0 {
+				return true
+			}
+			return false
+		},
+		120*time.Second,
+		time.Second,
+		"list remote validators failed: %v",
+		qRsp,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	flag.StringVar(&wasmContractPath, "contracts-path", "../testdata", "Set path to dir with gzipped wasm contracts")
+	flag.BoolVar(&wasmContractGZipped, "gzipped", true, "Use `.gz` file ending when set")
+	flag.StringVar(&configFile, "config", "../configs/starship.yaml", "starship config file for the infra")
+	flag.Parse()
+
+	err := MeshSecurity()
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ tests/starship/setup/setup.go
@@ -0,0 +1,288 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	_ "github.com/cosmology-tech/starship/clients/go/client"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/osmosis-labs/mesh-security-sdk/x/meshsecurity/types"
+)
+
+type Query map[string]map[string]any
+
+func Querier(chain *Client) func(contract string, query Query) map[string]any {
+	return func(contract string, query Query) map[string]any {
+		qRsp := make(map[string]any)
+		err := Eventually(
+			func() bool {
+				qRsp = make(map[string]any)
+				err := SmartQuery(chain, contract, query, &qRsp)
+				if err != nil {
+					if strings.Contains(err.Error(), "Value is already write locked") {
+						fmt.Printf("smart query error (ignored): %s\n", err)
+						return false
+					}
+					panic(fmt.Sprintf("error in query: %s\n. Stopping early...", err))
+				}
+				_, ok := qRsp["locked"]
+				if ok {
+					return false
+				}
+				return true
+			},
+			180*time.Second,
+			time.Second,
+			"locked contract response for too long: %v",
+			qRsp,
+		)
+		if err != nil {
+			panic(err)
+		}
+		return qRsp
+	}
+}
+
+type ProviderClient struct {
+	chain     *Client
+	contracts *ProviderContracts
+}
+
+func NewProviderClient(chain *Client) *ProviderClient {
+	return &ProviderClient{chain: chain}
+}
+
+type ProviderContracts struct {
+	vault                 sdk.AccAddress
+	nativeStakingContract sdk.AccAddress
+	externalStaking       sdk.AccAddress
+}
+
+func (p *ProviderClient) BootstrapContracts(connId, portID string) (*ProviderContracts, error) {
+	vaultCodeResp, err := StoreCodeFile(p.chain, buildPathToWasm("mesh_vault.wasm"))
+	if err != nil {
+		return nil, err
+	}
+	vaultCodeID := vaultCodeResp.CodeID
+	proxyCodeResp, err := StoreCodeFile(p.chain, buildPathToWasm("mesh_native_staking_proxy.wasm"))
+	if err != nil {
+		return nil, err
+	}
+	proxyCodeID := proxyCodeResp.CodeID
+	nativeStakingCodeResp, err := StoreCodeFile(p.chain, buildPathToWasm("mesh_native_staking.wasm"))
+	nativeStakingCodeID := nativeStakingCodeResp.CodeID
+
+	nativeInitMsg := []byte(fmt.Sprintf(`{"denom": %q, "proxy_code_id": %d}`, "ustake", proxyCodeID))
+	initMsg := []byte(fmt.Sprintf(`{"denom": %q, "local_staking": {"code_id": %d, "msg": %q}}`, "ustake", nativeStakingCodeID, base64.StdEncoding.EncodeToString(nativeInitMsg)))
+	contracts, err := InstantiateContract(p.chain, vaultCodeID, initMsg)
+	if err != nil {
+		return nil, err
+	}
+
+	vaultContract := contracts[0]
+	nativeStakingContract := contracts[1]
+
+	// external staking
+	unbondingPeriod := 21 * 24 * 60 * 60 // 21 days - make configurable?
+	extStaking, err := StoreCodeFile(p.chain, buildPathToWasm("external_staking.wasm"))
+	if err != nil {
+		return nil, err
+	}
+	extStakingCodeID := extStaking.CodeID
+	rewardToken := "ustake" // ics20 token
+	initMsg = []byte(fmt.Sprintf(
+		`{"remote_contact": {"connection_id":%q, "port_id":%q}, "denom": %q, "vault": %q, "unbonding_period": %d, "rewards_denom": %q}`,
+		connId, portID, "ustake", vaultContract.String(), unbondingPeriod, rewardToken))
+	externalStakingContracts, err := InstantiateContract(p.chain, extStakingCodeID, initMsg)
+	if err != nil {
+		return nil, err
+	}
+	externalStakingContract := externalStakingContracts[0]
+
+	r := &ProviderContracts{
+		vault:                 vaultContract,
+		externalStaking:       externalStakingContract,
+		nativeStakingContract: nativeStakingContract,
+	}
+	fmt.Printf("Provider Contracts:\n  valut: %s\n  externalStaking: %s\n  nativeStaking: %s\n", r.vault.String(), r.externalStaking.String(), r.nativeStakingContract.String())
+	p.contracts = r
+	return r, nil
+}
+
+func (p ProviderClient) MustExecVault(payload string, funds ...sdk.Coin) (*sdk.TxResponse, error) {
+	return p.mustExec(p.contracts.vault, payload, funds)
+}
+
+func (p ProviderClient) MustExecExtStaking(payload string, funds ...sdk.Coin) (*sdk.TxResponse, error) {
+	return p.mustExec(p.contracts.externalStaking, payload, funds)
+}
+
+func (p ProviderClient) mustExec(contract sdk.AccAddress, payload string, funds []sdk.Coin) (*sdk.TxResponse, error) {
+	rsp, err := p.chain.Client.SendMsg(context.Background(), &wasmtypes.MsgExecuteContract{
+		Sender:   p.chain.Address,
+		Contract: contract.String(),
+		Msg:      []byte(payload),
+		Funds:    funds,
+	}, "")
+	if err != nil {
+		return nil, err
+	}
+	return rsp, nil
+}
+
+func (p ProviderClient) MustFailExecVault(payload string, funds ...sdk.Coin) error {
+	return p.mustFailExec(p.contracts.vault, payload, funds)
+}
+
+// This will execute the contract and assert that it fails, returning the error if desired.
+// (Unlike most functions, it will panic on failure, returning error is success case)
+func (p ProviderClient) mustFailExec(contract sdk.AccAddress, payload string, funds []sdk.Coin) error {
+	resp, err := p.chain.Client.SendMsg(context.Background(), &wasmtypes.MsgExecuteContract{
+		Sender:   p.chain.Address,
+		Contract: contract.String(),
+		Msg:      []byte(payload),
+		Funds:    funds,
+	}, "")
+	if err != nil {
+		fmt.Printf("error wasm exec: error: %s, response: %v\n", err, resp)
+	}
+
+	return err
+}
+
+func (p ProviderClient) QueryExtStaking(q Query) map[string]any {
+	return Querier(p.chain)(p.contracts.externalStaking.String(), q)
+}
+
+func (p ProviderClient) QueryVault(q Query) map[string]any {
+	return Querier(p.chain)(p.contracts.vault.String(), q)
+}
+
+func (p ProviderClient) QueryVaultFreeBalance() int {
+	qRsp := map[string]any{}
+	err := Eventually(
+		func() bool {
+			qRsp = p.QueryVault(Query{
+				"account": {"account": p.chain.Address},
+			})
+			_, ok := qRsp["locked"]
+			if ok {
+				return false
+			}
+			return true
+		},
+		60*time.Second,
+		time.Second,
+		"valut token locked for too long: %v",
+		qRsp,
+	)
+	if err != nil {
+		panic(err)
+	}
+	acct := qRsp["account"].(map[string]any)
+	r, err := strconv.Atoi(acct["free"].(string))
+	if err != nil {
+		panic(err)
+	}
+	return r
+}
+
+type ConsumerClient struct {
+	chain     *Client
+	contracts *ConsumerContract
+}
+
+func NewConsumerClient(chain *Client) *ConsumerClient {
+	return &ConsumerClient{chain: chain}
+}
+
+type ConsumerContract struct {
+	staking   sdk.AccAddress
+	priceFeed sdk.AccAddress
+	converter sdk.AccAddress
+}
+
+func (p *ConsumerClient) BootstrapContracts() (*ConsumerContract, error) {
+	// what does this do????
+	// modify end-blocker to fail fast in tests
+	//msModule := p.app.ModuleManager.Modules[types.ModuleName].(*meshsecurity.AppModule)
+	//msModule.SetAsyncTaskRspHandler(meshsecurity.PanicOnErrorExecutionResponseHandler())
+
+	code, err := StoreCodeFile(p.chain, buildPathToWasm("mesh_simple_price_feed.wasm"))
+	if err != nil {
+		return nil, err
+	}
+	codeID := code.CodeID
+
+	initMsg := []byte(fmt.Sprintf(`{"native_per_foreign": "%s"}`, "0.5")) // todo: configure price
+	priceFeedContracts, err := InstantiateContract(p.chain, codeID, initMsg)
+	if err != nil {
+		return nil, err
+	}
+	priceFeedContract := priceFeedContracts[0]
+
+	// virtual staking is setup by the consumer
+	virtStakeCode, err := StoreCodeFile(p.chain, buildPathToWasm("mesh_virtual_staking.wasm"))
+	if err != nil {
+		return nil, err
+	}
+	virtStakeCodeID := virtStakeCode.CodeID
+
+	// instantiate converter
+	code, err = StoreCodeFile(p.chain, buildPathToWasm("mesh_converter.wasm"))
+	if err != nil {
+		return nil, err
+	}
+	codeID = code.CodeID
+
+	discount := "0.1" // todo: configure price
+	initMsg = []byte(fmt.Sprintf(`{"price_feed": %q, "discount": %q, "remote_denom": %q,"virtual_staking_code_id": %d}`,
+		priceFeedContract.String(), discount, "ustake", virtStakeCodeID))
+	// bug in lens that returns second contract instantiated
+	contracts, err := InstantiateContract(p.chain, codeID, initMsg)
+	if err != nil {
+		return nil, err
+	}
+	converterContract, virtualStakingContract := contracts[0], contracts[1]
+
+	r := &ConsumerContract{
+		staking:   virtualStakingContract,
+		priceFeed: priceFeedContract,
+		converter: converterContract,
+	}
+	fmt.Printf("Consumer Contracts:\n  staking: %s\n  priceFeed: %s\n  converter: %s\n", r.staking.String(), r.priceFeed.String(), r.converter.String())
+	p.contracts = r
+	return r, nil
+}
+
+func (p *ConsumerClient) ExecNewEpoch() {
+	// wait for epoch length
+	time.Sleep(150 * time.Second)
+}
+
+// MustExecGovProposal submit and vote yes on proposal
+func (p *ConsumerClient) MustExecGovProposal(msg *types.MsgSetVirtualStakingMaxCap) {
+	proposalID, err := submitGovProposal(p.chain, msg)
+	if err != nil {
+		panic(err)
+	}
+	err = voteAndPassGovProposal(p.chain, proposalID)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (p *ConsumerClient) QueryMaxCap() types.QueryVirtualStakingMaxCapLimitResponse {
+	q := &types.QueryVirtualStakingMaxCapLimitRequest{Address: p.contracts.staking.String()}
+	rsp, err := types.NewQueryClient(p.chain.Client).VirtualStakingMaxCapLimit(context.Background(), q)
+	if err != nil {
+		panic(err)
+	}
+	return *rsp
+}
