e71cb9e8835d1a9e5fc9aa1f3f8622d00420de47
Anmol1696 <anmol1696@gmail.com>
2023-07-13 14:57:01 +0200

    Fix setup command and update cmd main file

--- tests/starship/setup/cmd/main.go
+++ tests/starship/setup/cmd/main.go
@@ -12,9 +12,9 @@ var (
 )
 
 func main() {
-	flag.StringVar(&WasmContractPath, "Contracts-path", "../../testdata", "Set path to dir with gzipped wasm Contracts")
+	flag.StringVar(&WasmContractPath, "Contracts-path", "../testdata", "Set path to dir with gzipped wasm Contracts")
 	flag.BoolVar(&WasmContractGZipped, "gzipped", true, "Use `.gz` file ending when set")
-	flag.StringVar(&ConfigFile, "config", "../../configs/starship.yaml", "starship config file for the infra")
+	flag.StringVar(&ConfigFile, "config", "../configs/starship.yaml", "starship config file for the infra")
 	flag.Parse()
 
 	_, _, err := setup.MeshSecurity(ConfigFile, WasmContractPath, WasmContractGZipped)
