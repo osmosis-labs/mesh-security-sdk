f1ac3df5bd09e6023e4274eb06257586fbf17c5e
Dzung Do <neitdung@gmail.com>
2024-07-14 23:18:50 +0700

    fix MsgUpdateParams

--- x/meshsecurityprovider/types/codec.go
+++ x/meshsecurityprovider/types/codec.go
@@ -11,11 +11,13 @@ import (
 // RegisterLegacyAminoCodec registers the necessary x/meshsecurityprovider interfaces and concrete types
 // on the provided LegacyAmino codec. These types are used for Amino JSON serialization.
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgUpdateParams{}, "meshsecurityprovider/MsgUpdateParams", nil)
 }
 
 func RegisterInterfaces(registry types.InterfaceRegistry) {
 	registry.RegisterImplementations(
 		(*sdk.Msg)(nil),
+		&MsgUpdateParams{},
 	)
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
